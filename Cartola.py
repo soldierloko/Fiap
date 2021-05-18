3f2b1e8a62abe420942a98eb58142d3f5bd516a1
Bruno Mesquita <63270404+soldierloko@users.noreply.github.com>
2021-05-18 19:09:25 -0300

    Last

---
+++ Cartola.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import os
+import sys
+from tkinter import filedialog
+from tkinter import *
+
+def valida_arquivo():
+    #Abre a janela FileDialog para selecionar o arquivo
+    filename = filedialog.askdirectory(title="Selecione o Local dos Arquivos: ")
+    #Verifica se o caminho foi selecionado 
+    if filename == '':
+        sys.exit()
+
+    return filename
+
+
+#Busca o caminho do diretório dos arquivos
+caminho = valida_arquivo()
+
+
+#Cria o DF de saída
+df_final = pd.DataFrame(columns=('atletas.nome'
+                        ,'atletas.slug'
+                        ,'atletas.apelido'
+                        ,'atletas.foto'
+                        ,'atletas.atleta_id'
+                        ,'atletas.rodada_id'
+                        ,'atletas.clube_id'
+                        ,'atletas.posicao_id'
+                        ,'atletas.status_id'
+                        ,'atletas.pontos_num'
+                        ,'atletas.preco_num'
+                        ,'G'
+                        ,'I'
+                        ,'RB'
+                        ,'CA'
+                        ,'PE'
+                        ,'A'
+                        ,'SG'
+                        ,'DD'  
+                        ,'FT'  
+                        ,'GS'  
+                        ,'CV'  
+                        ,'GC'))
+
+#Coloca o caminho dos arquivos um uma lista
+for nome in os.listdir(caminho):
+
+    #verifica se é um Excel
+    if '.csv' in nome:
+        
+        #Atribui o arquivo ao DataFrame
+        df = pd.read_csv(caminho + "/" + nome)
+
+        #Faz o Merge entre os DataFrames
+        frames = [df_final,df]
+        df_final = pd.concat(frames)
+        
+df_final.to_csv(caminho + "/" +"Rodadas_2018.csv")  
+print('Concluido com Sucesso!')    
--- Teste.py
+++ Teste.py
@@ -5,32 +5,19 @@
 
 
 
-import cartolafc
-
-api = cartolafc.Api(email='email@email.com', password='s3nh4', attempts=5, redis_url='redis://localhost:6379/0')
-
-print(api.amigos())
-print(api.clubes())
-print(api.liga(nome='Virtus Premier League'))
-print(api.liga(nome='Nacional', page=2))
-print(api.liga(nome='Nacional', page=3, order_by=cartolafc.RODADA))
-print(api.liga(slug='virtus-premier-league'))
-print(api.ligas(query='Virtus'))
-print(api.ligas_patrocinadores())
-print(api.mercado())
-print(api.mercado_atletas())
-print(api.parciais())
-print(api.partidas(1))
-print(api.pontuacao_atleta(81682))
-print(api.pos_rodada_destaques())
-print(api.time(id=2706236))
-print(api.time(id=2706236, as_json=True))
-print(api.time(nome='ALCAFLA FC'))
-print(api.time(nome='ALCAFLA FC', as_json=True))
-print(api.time(slug='alcafla-fc'))
-print(api.time(slug='alcafla-fc', as_json=True))
-print(api.time_logado())
-print(api.time_parcial(id=2706236))
-print(api.time_parcial(nome='ALCAFLA FC'))
-print(api.time_parcial(slug='alcafla-fc'))
-print(api.times(query='Faly'))
+import json, mechanize
+ 
+# br será o objeto que simula o browser
+br = mechanize.Browser()
+ 
+# Abre a página de login Cartola através do mechanize
+br.open('https://loginfree.globo.com/login/438')
+ 
+#Na página, selecionamos o primeiro form presente. 'nr=0' indica que estamos selecionando o form de índice 0 dentre os encontrados.
+#Analisando a página, vemos que realmente só há um form.
+#Após selecionarmos o form, preenchemos os campos com username e senha que permitam fazer o login.
+ 
+br.select_form(nr=0)
+br.form['login-passaporte'] = 'SEU_EMAIL'
+br.form['senha-passaporte'] = 'SUA_SENHA'
+br.submit()
